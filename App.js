ca0b3014b4547dd11e81fb6528dbdd872fea8738
Robert Balonek <robertbalonek@Roberts-MBP.attlocal.net>
2020-10-23 11:22:31 -0700

    Set State

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, TouchableHighlight, Image } from 'react-native'
 
 
 export default function App() {
-  let [joke, setJoke] = React.useState('')
+  let [joke, setJoke] = React.useState('Click GET JOKE!')
 
   const fetchApiCall = () => {
     fetch("https://icanhazdadjoke.com/", {
